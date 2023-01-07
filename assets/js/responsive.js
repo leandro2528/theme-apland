19fc57069a23f00292fad18e6ce9e7275c244ff1
leandro2528 <122063177+leandro2528@users.noreply.github.com>
2023-01-06 21:16:44 -0300

    Add files via upload

---
+++ assets/js/responsive.js
@@ -0,0 +1,12 @@
+function menuOpened() {
+    if(document.querySelector('nav').style.display = 'none') {
+        document.querySelector('nav').style.display = 'block';
+    } 
+}
+function menuClosed() {
+    document.querySelector('nav').style.display = 'none';
+}
+function link() {
+    document.querySelector('nav').style.display = 'none';
+}
+
